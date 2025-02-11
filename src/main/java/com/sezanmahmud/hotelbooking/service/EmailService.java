9580a2958c7865852a5dd4cfa83ff893a2ffcfad
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-02-11 23:12:20 +0600

    -->>

---
+++ src/main/java/com/sezanmahmud/hotelbooking/service/EmailService.java
@@ -0,0 +1,31 @@
+package com.sezanmahmud.hotelbooking.service;
+
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+
+    public void sendSimpleMail(String to, String subject, String body) throws MessagingException {
+
+        MimeMessage massage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(massage, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body);
+
+        javaMailSender.send(massage);
+    }
+}
