88f76c405cd90085a70dda9b734507b194628bce
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-02-18 00:47:39 +0600

    Class 10

--- src/main/java/com/sezanmahmud/hotelbooking/entity/Token.java
+++ src/main/java/com/sezanmahmud/hotelbooking/entity/Token.java
@@ -18,4 +18,47 @@ public class Token {
     @ManyToOne
     @JoinColumn(name ="user_id")
     private User user;
+
+
+    public Token() {
+    }
+
+    public Token(long id, String token, boolean logout, User user) {
+        this.id = id;
+        this.token = token;
+        this.logout = logout;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isLogout() {
+        return logout;
+    }
+
+    public void setLogout(boolean logout) {
+        this.logout = logout;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/sezanmahmud/hotelbooking/jwt/JwtService.java
+++ src/main/java/com/sezanmahmud/hotelbooking/jwt/JwtService.java
@@ -3,15 +3,16 @@ package com.sezanmahmud.hotelbooking.jwt;
 import com.sezanmahmud.hotelbooking.entity.User;
 import com.sezanmahmud.hotelbooking.repository.TokenRepository;
 import io.jsonwebtoken.Claims;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -54,6 +55,50 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis()+24*60*60*1000))      //Set Token Expire Time
                 .signWith(getsigninKey())                                               // Sign the Token with Secret key
                 .compact();                                                             // Build and compacts the token into String
+    }
+
+    public String extractUserName(String token){
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    private boolean isTokenExpired(String token){
+
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isValid(String token, UserDetails user) {
+
+        String userName = extractUserName(token);
+
+        boolean validToken = tokenRepository
+                .findByToken(token)
+                .map(t -> !t.isLogout())
+                .orElse(false);
+
+        return (userName.equals(user.getUsername()) && !isTokenExpired(token) && validToken);
+
+    }
+
+        // Extract a specific Calim from the token Claims
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver ) {
+
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    //get User Role From Token
+
+    public String extrcatUserRole(String token){
+
+        return extractClaim(token, claims-> claims.get("role",String.class));
     }
 
 
