1136c8019dfe5cc0da74588e44d58a0f21be369b
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-03-10 00:05:18 +0600

    -->>

--- src/main/java/com/sezanmahmud/hotelbooking/security/SecurityConfig.java
+++ src/main/java/com/sezanmahmud/hotelbooking/security/SecurityConfig.java
@@ -7,10 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,9 +30,31 @@ public class SecurityConfig {
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return
+                http
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .cors(Customizer.withDefaults())
+                        .authorizeHttpRequests(
+                                req->
+                                        req.requestMatchers("/login","register")
+                                                .permitAll()
+                                                .requestMatchers("/api/**")
+                                                .hasAnyAuthority("USER")
+                        )
+                        .userDetailsService(userService)
+                        .sessionManagement(
+                                session->
+                                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+                        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                        .build();
+
+    }
 
 
-    @Bean
+     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
